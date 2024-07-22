a2042e3af88767cc421ba9d096599d770875d5d3
Vladimir Rodionov <vladrodionov@gmail.com>
2024-07-21 19:32:32 -0700

    Set default statistics output interval to 300 seconds #54

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -8,7 +8,7 @@ export MEMCARROT_VERSION=0.10
 export MEMCARROT_RELEASE=memcarrot-${MEMCARROT_VERSION}-all.jar
 export MEMCARROT_APPS_PARAMS="./conf/memcarrot.cfg"
 export MEMCARROT_INSTANCE_NAME=I_$(pwd)
-export MEMCARROT_APP_OPTS="-Dlocation=${MEMCARROT_INSTANCE_NAME} -Dlog4j2.configurationFile=../conf/log4j2.xml"
+export MEMCARROT_APP_OPTS="-DSTATS_TASK -DSTATS_TASK_INTERVAL=300 -Dlocation=${MEMCARROT_INSTANCE_NAME} -Dlog4j2.configurationFile=../conf/log4j2.xml"
 # Ubuntu jemalloc path
 # export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libjemalloc.so
 # export MALLOC_CONF=prof:true,lg_prof_interval:30,lg_prof_sample:17
