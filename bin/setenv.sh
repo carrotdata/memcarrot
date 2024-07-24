f68bd37e8616067db4d8ca117896bc72d9010d46
Konstantin Zheludkov <koss@Konstantins-MacBook-Pro.local>
2024-07-24 09:00:14 -0700

    after merge

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -3,10 +3,9 @@
 # Yeah, let set individual JAVA_HOME in .bashrc ?
 # JAVA_HOME variable could be set on stand alone not-dev server.
 #export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-11.0.11.jdk/Contents/Home
-export MEMCARROT_VERSION="memcarrot-0.10"
+export MEMCARROT_VERSION="memcarrot-0.11"
 export MAX_HEAP_SIZE=100m
-export MEMCARROT_VERSION=0.11
-export MEMCARROT_RELEASE=lib/carrotdata-memcarrot-${MEMCARROT_VERSION}-bundle.jar
+export MEMCARROT_RELEASE=lib/carrotdata-${MEMCARROT_VERSION}-bundle.jar
 export MEMCARROT_APPS_PARAMS="./conf/memcarrot.cfg"
 export MEMCARROT_INSTANCE_NAME=I_$(pwd)
 export MEMCARROT_APP_OPTS="-DMEMCARROT_VERSION=${MEMCARROT_VERSION} -DSTATS_TASK -DSTATS_TASK_INTERVAL=300 -Dlocation=${MEMCARROT_INSTANCE_NAME} -Dlog4j2.configurationFile=./conf/log4j2.xml"
