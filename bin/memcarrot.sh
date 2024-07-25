a5090a1fa8f5f5ffcd5d0e933786df0b0aa6c31b
Konstantin Zheludkov <koss@Konstantins-MacBook-Pro.local>
2024-07-24 23:09:09 -0700

    test JAVA_HOME

--- bin/memcarrot.sh
+++ bin/memcarrot.sh
@@ -12,6 +12,12 @@ cd "${APP_DIR}" || exit
 
 . ./bin/setenv.sh
 
+if [ -z "${JAVA_HOME}" ]; then
+  echo "Please set the environment variables JAVA_HOME"
+  exit 1
+fi
+
+#===== set JVM options =====
 export JVM_OPTS="-Xmx${MAX_HEAP_SIZE} -XX:MaxDirectMemorySize=256m --add-opens java.base/jdk.internal.misc=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED \
         --add-opens java.base/java.security=ALL-UNNAMED --add-opens jdk.unsupported/sun.misc=ALL-UNNAMED \
         --add-opens java.base/sun.security.action=ALL-UNNAMED --add-opens jdk.naming.rmi/com.sun.jndi.rmi.registry=ALL-UNNAMED \
