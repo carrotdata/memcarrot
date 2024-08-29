5785878459c4c01f73836798d27e86f740c5cd08
Vladimir Rodionov <vladrodionov@gmail.com>
2024-08-29 09:12:54 -0700

    fixed version in setenv.sh

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -3,7 +3,7 @@
 # Yeah, let set individual JAVA_HOME in .bashrc ?
 # JAVA_HOME variable could be set on stand alone not-dev server.
 #export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-11.0.11.jdk/Contents/Home
-export MEMCARROT_VERSION="memcarrot-0.11"
+export MEMCARROT_VERSION="memcarrot-0.12"
 export MAX_HEAP_SIZE=200m
 export MEMCARROT_RELEASE=lib/${MEMCARROT_VERSION}-bundle.jar
 export MEMCARROT_APPS_PARAMS="./conf/memcarrot.cfg"
