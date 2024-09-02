062b1cdf42147c26f2f1cc11e0459c3b57b73316
Vladimir Rodionov <vladrodionov@gmail.com>
2024-09-02 15:56:46 -0700

    Bump carrot-cache version to 0.12.2 #72

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -3,15 +3,9 @@
 # Yeah, let set individual JAVA_HOME in .bashrc ?
 # JAVA_HOME variable could be set on stand alone not-dev server.
 #export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-11.0.11.jdk/Contents/Home
-export MEMCARROT_VERSION="memcarrot-0.12"
+export MEMCARROT_VERSION="memcarrot-0.12.2"
 export MAX_HEAP_SIZE=200m
 export MEMCARROT_RELEASE=lib/${MEMCARROT_VERSION}-bundle.jar
 export MEMCARROT_APPS_PARAMS="./conf/memcarrot.cfg"
 export MEMCARROT_INSTANCE_NAME=I_$(pwd)
 export MEMCARROT_APP_OPTS="-DMEMCARROT_VERSION=${MEMCARROT_VERSION} -DSTATS_TASK -DSTATS_TASK_INTERVAL=30 -Dlocation=${MEMCARROT_INSTANCE_NAME} -Dlog4j2.configurationFile=./conf/log4j2.xml"
-# Ubuntu jemalloc path
-# export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libjemalloc.so
-# export MALLOC_CONF=prof:true,lg_prof_interval:30,lg_prof_sample:17
-# jeprof --show_bytes --gif /path/to/jvm/bin/java jeprof*.heap > /tmp/app-profiling.gif
-# Mac jemalloc path
-# export LD_PRELOAD=
