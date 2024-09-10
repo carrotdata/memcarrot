b4f1254a1f4fc8f2175ad00b57d5e4b523fab8a6
Konstantin Zheludkov <koss@Konstantins-MacBook-Pro.local>
2024-09-09 20:15:01 -0700

    add start existing container

---
+++ bin/memcarrot-docker-start.sh
@@ -0,0 +1,71 @@
+: '
+  This script is used to run the memcarrot docker image.
+'
+
+show_help() {
+    echo "Usage: memcarrot-docker-stop.sh [--name NAME] [--help]"
+    echo
+    echo "   -n, --image name Specify the container name 'carrotdata/memcarrot'"
+    echo "   -h, --help       Display this help message"
+}
+
+# Initialize variables
+image_name=""
+
+# Parse short options with getopts
+while getopts ":h:n:" opt; do
+    case ${opt} in
+        h )
+            show_help
+            exit 0
+            ;;
+        n )
+            image_name=$OPTARG
+            ;;
+        \? )
+            echo "Invalid option: $OPTARG" 1>&2
+            show_help
+            exit 1
+            ;;
+        : )
+            echo "Invalid option: $OPTARG requires an argument" 1>&2
+            show_help
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND -1))
+
+# Manually parse long options
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --help)
+            show_help
+            exit 0
+            ;;
+        --iname_name)
+            if [[ -n $2 ]]; then
+                image_name=$2
+                shift 2
+            else
+                echo "Error: --version requires an argument." 1>&2
+                show_help
+                exit 1
+            fi
+            ;;
+        *)
+            echo "Invalid option: $1" 1>&2
+            show_help
+            exit 1
+            ;;
+    esac
+done
+
+if [ -z "$image_name" ]; then
+    echo "Please provide the --image_name of memcarrot"
+    show_help
+    exit 1
+fi
+
+docker start ${image_name}
+
