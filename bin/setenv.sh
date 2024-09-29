714044a5850bb2eb3aff168f684efcb8e58826e9
Vladimir Rodionov <vladrodionov@gmail.com>
2024-09-29 13:32:14 -0700

    Release 0.15.1 #86

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -4,7 +4,7 @@
 # JAVA_HOME variable could be set on stand alone not-dev server.
 #export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-11.0.11.jdk/Contents/Home
 export MAX_HEAP_SIZE=200m
-export MEMCARROT_RELEASE=lib/memcarrot-0.15-bundle.jar
+export MEMCARROT_RELEASE=lib/memcarrot-0.15.1-bundle.jar
 export MEMCARROT_APPS_PARAMS="./conf/memcarrot.cfg"
 export MEMCARROT_INSTANCE_NAME=I_$(pwd)
 export MEMCARROT_APP_OPTS="-DSTATS_TASK -DSTATS_TASK_INTERVAL=30 -Dlocation=${MEMCARROT_INSTANCE_NAME} -Dlog4j2.configurationFile=./conf/log4j2.xml"
