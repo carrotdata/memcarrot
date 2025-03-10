640d55df5a0be950dd06cb092608b12db63b6884
Konstantin Zheludkov <kzheludkov@gmail.com>
2025-03-09 18:09:17 -0700

    code refactoring for grafana settings

--- bin/setenv_4docker.sh
+++ bin/setenv_4docker.sh
@@ -1,5 +1,7 @@
 
 export MEMCARROT_APP_OPTS="-Xmx200m -XX:MaxDirectMemorySize=256m -DSTATS_TASK -DSTATS_TASK_INTERVAL=30 \
 -Dlog4j2.configurationFile=/users/carrotdata/memcarrot/conf/log4j2.xml"
-export JMX_EXPORTER_ENABLED=false
+if [ -z "${JMX_EXPORTER_ENABLED}" ]; then
+  export JMX_EXPORTER_ENABLED=false
+fi
 export JMX_EXPORTER="-javaagent:/users/carrotdata/memcarrot/lib/jmx_prometheus_javaagent-1.0.1.jar=9191:/users/carrotdata/memcarrot/conf/jmxconfig.yml"
